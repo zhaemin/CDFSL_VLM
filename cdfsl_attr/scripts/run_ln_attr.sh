ac1be8b7901efc25bc5ec3a575b450f2c455fc13
zhaemin <95492807+zhaemin@users.noreply.github.com>
2025-10-27 13:53:07 +0900

    Update run_ln_attr.sh

--- cdfsl_attr/scripts/run_ln_attr.sh
+++ cdfsl_attr/scripts/run_ln_attr.sh
@@ -1,13 +1,14 @@
 ROOT_PATH=$1
 NUM_ATTR=$2
 
-for DATA in fgvc
+# imagenet sun397 fgvc eurosat stanford_cars food101 oxford_pets oxford_flowers caltech101 dtd ucf101
+for DATA in imagenet sun397 fgvc eurosat stanford_cars food101 oxford_pets oxford_flowers caltech101 dtd ucf101
 do
     for SHOOT in 4
     do
         for SEED in 1
         do
-        CUDA_VISIBLE_DEVICES=3 python main.py --shots ${SHOOT} --dataset ${DATA} --seed ${SEED} --mode 'ln_only_attr' --exp_name "ln_attr8" --root_path ${ROOT_PATH} --num_attr ${NUM_ATTR}
+        CUDA_VISIBLE_DEVICES=3 python main.py --shots ${SHOOT} --dataset ${DATA} --seed ${SEED} --mode 'ln_only_attr' --exp_name "ln_attr${NUM_ATTR}" --root_path ${ROOT_PATH} --num_attr ${NUM_ATTR}
         done
     done
 done
